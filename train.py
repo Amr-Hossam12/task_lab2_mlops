ac87b2c6510d5dfc2068ab0f129564ce35a8ebfc
amr <amrhosam1212@gmail.com>
2025-10-15 23:29:34 +0300

    updated train script for the SVC model

---
+++ train.py
@@ -0,0 +1,15 @@
+import os
+import joblib
+import pandas as pd
+from sklearn.svm import SVC
+
+train_df = pd.read_csv('data/train.csv')
+X_train = train_df.drop('left', axis=1)
+y_train = train_df['left']
+
+model = SVC(kernel='rbf', C=1.0, gamma='scale', random_state=42)
+model.fit(X_train, y_train)
+
+os.makedirs('models', exist_ok=True)
+joblib.dump(model, 'models/model.pkl')
+print("SVC model saved to models/model.pkl")
