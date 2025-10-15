fb3ca1c8e210fa4c71cd7d5603a90b235d46728a
amr <amrhosam1212@gmail.com>
2025-10-15 23:46:15 +0300

    updated SVC modell parameters

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ train_df = pd.read_csv('data/train.csv')
 X_train = train_df.drop('left', axis=1)
 y_train = train_df['left']
 
-model = SVC(kernel='rbf', C=1.0, gamma='scale', random_state=42)
+model = SVC(kernel='poly', C=1.0, gamma='scale', random_state=42)
 model.fit(X_train, y_train)
 
 os.makedirs('models', exist_ok=True)
